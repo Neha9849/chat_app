dbe4bbf185e6a4dc17b4bdcffae69ac0fbde0258
Neha9849 <nehadeekonda66@gmail.com>
2022-06-08 02:46:13 +0530

    update

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./../Home/home.css";
+import "./chat.css"
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <div className="chat">
+      <nav>
+        <div className="home">
+          <div className="nav">
+            <Link to="/">
+              <h3 className="px-5">
+                <i className="fa-solid fa-comment tprimary px-2"></i>
+                ChitChat
+              </h3>
+            </Link>
+            <Link to="/">
+              <button className="btn btn-primary btnSecondary mx-5" onClick={()=>{console.log('user disconneted')}}>
+                Leave
+              </button>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <div className="chatContainer flexc">
 
-export default Chat
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
